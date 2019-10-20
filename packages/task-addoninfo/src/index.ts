e517fe4e157ada81c758ad66c0ac355463d0a3f8
ark120202 <ark120202@gmail.com>
2019-10-20 13:31:34 +0500

    feat(task-maps): remove map modifications support

--- packages/task-addoninfo/src/index.ts
+++ packages/task-addoninfo/src/index.ts
@@ -2,11 +2,6 @@ import { Task, TaskState } from '@eaglesong/helper-task';
 import _ from 'lodash';
 import PQueue from 'p-queue';
 
-export interface AddonInfoMap {
-  MaxPlayers: number;
-  TeamCount: number;
-}
-
 export interface DefaultKey {
   Key: string;
   Command: string;
@@ -39,16 +34,11 @@ export interface Options {
   EnablePickRules?: boolean;
   /**
    * Binds keys to console commands.
-   *
-   * Prefer to use `Game.CreateCustomKeyBind` whenever possible.
    */
   Default_Keys?: DefaultKey[];
 }
 
 export default class AddonInfoTask extends Task<Options> {
-  private readonly queue = new PQueue({ concurrency: 1 });
-  private maps: Record<string, AddonInfoMap> = {};
-
   constructor(options: Options = {}) {
     super(options);
   }
@@ -57,12 +47,14 @@ export default class AddonInfoTask extends Task<Options> {
     this.hooks.build.tapPromise(this.constructor.name, () => this.queueEmit());
   }
 
-  public setMaps(maps: Record<string, AddonInfoMap>) {
+  private maps: Record<string, any> = {};
+  public setMaps(maps: Record<string, any>) {
     this.maps = maps;
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
     this.queueEmit();
   }
 
+  private readonly queue = new PQueue({ concurrency: 1 });
   private async queueEmit() {
     return this.queue.add(async () => {
       if (this.state !== TaskState.Working) this.start();
@@ -73,25 +65,23 @@ export default class AddonInfoTask extends Task<Options> {
   }
 
   private async emit() {
-    const content: Record<string, any> = {
-      maps: Object.keys(this.maps).join(' '),
-      ...this.maps,
-    };
-
-    (Object.keys(this.options) as (keyof Options)[]).forEach(key => {
-      if (key === 'Default_Keys' && this.options.Default_Keys != null) {
-        content.Default_Keys = _.fromPairs(
-          // TODO: as const
-          this.options.Default_Keys.map((k, i): [string, DefaultKey] => [String(i + 1), k]),
-        );
-        return;
-      }
-
-      const value = this.options[key];
-      if (value != null) content[key] = value ? '1' : '0';
-    });
-
     if (this.dotaPath == null) return;
+
+    const content: Record<string, any> = { ...this.maps };
+
+    for (const [key, value] of Object.entries(this.options)) {
+      if (value == null) return;
+
+      if (key === 'Default_Keys') {
+        content[key] = _.fromPairs(
+          // TODO: as const
+          (value as DefaultKey[]).map((k, i): [string, DefaultKey] => [String(i + 1), k]),
+        );
+      } else {
+        content[key] = value ? '1' : '0';
+      }
+    }
+
     await this.outputKV1(this.resolvePath('game', 'addoninfo.txt'), { '': content });
   }
 }
--- packages/task-maps/src/metadata.schema.ts
+++ packages/task-maps/src/metadata.schema.ts
@@ -2,55 +2,11 @@ import schema from './metadata.schema.json';
 
 export { schema };
 
-export interface MapData {
-  /**
-   * @TJS-type integer
-   * @minimum 0
-   */
-  teams: number;
-
-  /**
-   * @TJS-type integer
-   * @minimum 0
-   */
-  players: number;
-
-  /**
-   * A list of map modifications, used to construct combinations.
-   * `_` will evaluate to name without any modifier.
-   *
-   * @minItems 1
-   * @uniqueItems true
-   * @default ["_"]
-   */
-  // modifications?: (string | { name: string; env: Record<string, any> })[];
-  modifications?: string[];
-
-  /**
-   * See @dotastd/env.
-   */
-  // env?: Record<string, any>;
-}
-
 /**
- * The information about maps which appears in addoninfo file.
+ * A list of playable maps and their extra data.
  */
-export interface MapsMetadata {
-  /**
-   * Order of maps as they appear in map list.
-   * Use `...` for all not specified maps.
-   *
-   * @example ["level_1", "...", "level_10"]
-   * @minItems 1
-   * @uniqueItems true
-   * @default ["..."]
-   */
-  order?: string[];
-
-  /**
-   * @validationKeywords ^\w+$
-   * @default _
-   */
-  separator?: string;
-  maps: Record<string, MapData>;
+export type MapsMetadata = Record<string, MapData>;
+export interface MapData {
+  MaxPlayers: number;
+  [key: string]: any;
 }
