117cb2a2fa86115f96148bc2fcc359ffba941d8f
ark120202 <ark120202@gmail.com>
2019-11-29 16:27:40 +0500

    feat(task-eslint): move eslint to a worker thread

--- packages/task-eslint/src/index.ts
+++ packages/task-eslint/src/index.ts
@@ -1,21 +1,38 @@
 import { TransformTask } from '@eaglesong/helper-task';
+import path from 'path';
+// eslint-disable-next-line node/no-unsupported-features/node-builtins
+import { Worker } from 'worker_threads';
 import { CLIEngine } from 'eslint';
 
-export interface Options {
-  extensions?: string[];
-}
-
+// TODO: It's not supposed to be a class
+export type Options = InstanceType<typeof CLIEngine.Options>;
 export default class ESLintTask extends TransformTask<Options> {
   private readonly extensions = this.options.extensions ?? ['.js', '.jsx', '.ts', '.tsx'];
-  private readonly cliEngine = new CLIEngine({ extensions: this.extensions });
   protected pattern = [`**/*{${this.extensions.join(',')}}`, '!node_modules'];
 
   constructor(options: Options = {}) {
     super(options);
   }
 
-  protected afterWatch() {
-    const { results } = this.cliEngine.executeOnFiles(['.']);
+  private worker!: Worker;
+  public apply() {
+    const workerData: CLIEngine.Options = {
+      extensions: this.extensions,
+      cwd: this.context,
+      ...this.options,
+    };
+
+    this.worker = new Worker(path.join(__dirname, 'worker.js'), { workerData });
+
+    super.apply();
+  }
+
+  protected async afterWatch() {
+    this.worker.postMessage(undefined);
+    const results = await new Promise<CLIEngine.LintResult[]>(resolve =>
+      this.worker.on('message', resolve),
+    );
+
     for (const { filePath, messages } of results) {
       for (const { severity, line, column, message } of messages) {
         if (severity !== 0) {
@@ -25,6 +42,10 @@ export default class ESLintTask extends TransformTask<Options> {
           this.error({ filePath, level, message: fullMessage });
         }
       }
+    }
+
+    if (!this.isWatching) {
+      await this.worker.terminate();
     }
   }
 }
