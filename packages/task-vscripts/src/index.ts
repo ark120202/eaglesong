d0d4682a427bd0feba0b1bd3ec25c9c8cf499e17
ark120202 <ark120202@gmail.com>
2019-12-01 21:57:37 +0500

    fix(task-vscripts): lualib is not emitted

--- packages/task-vscripts/src/index.ts
+++ packages/task-vscripts/src/index.ts
@@ -1,8 +1,13 @@
 import { Task } from '@eaglesong/helper-task';
+import fs from 'fs-extra';
 import path from 'path';
 // eslint-disable-next-line node/no-unsupported-features/node-builtins
 import { Worker } from 'worker_threads';
 import { Message, WorkerData } from './worker';
+
+const tstlPath = path.dirname(require.resolve('typescript-to-lua/package.json'));
+const copyLuaLib = (to: string) =>
+  fs.copy(path.join(tstlPath, 'dist/lualib/lualib_bundle.lua'), path.join(to, 'lualib_bundle.lua'));
 
 export default class VScriptsTask extends Task<void> {
   constructor() {
@@ -10,12 +15,18 @@ export default class VScriptsTask extends Task<void> {
   }
 
   public apply() {
-    this.hooks.build.tap(this.constructor.name, () => {
+    this.hooks.build.tapPromise(this.constructor.name, async () => {
+      let outDir: string | undefined;
+      if (this.dotaPath != null) {
+        outDir = this.resolvePath('game', 'scripts/vscripts');
+        await copyLuaLib(outDir);
+      }
+
       const workerData: WorkerData = {
         currentDirectory: this.resolvePath('src/vscripts'),
         configPath: this.resolvePath('src/vscripts/tsconfig.json'),
         isWatching: this.isWatching,
-        outDir: this.dotaPath != null ? this.resolvePath('game', 'scripts/vscripts') : undefined,
+        outDir,
 
         hasDota: this.dotaPath != null,
         commonRoot: this.resolvePath('src/common'),
